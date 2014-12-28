e0ef488f2433a97e63324282292bed1d5f600223
Iván Álvarez <ialvarezpereira@gmail.com>
2014-12-28 14:43:34 +0100

    Remove unused imports

--- src/ExampleModel/Product.cs
+++ src/ExampleModel/Product.cs
@@ -1,11 +1,6 @@
 ﻿using DotNetDbf;
 using DotNetDbf.Attributes;
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Linq.Expressions;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace ExampleModel
 {
